be2039def6c7aaaf491a8966d9e301d82921e9c0
DSenen <davidsenen@hotmail.com>
2020-08-26 14:01:50 +0100

    I have solved the problem with the Google Map. Now it should work.

--- js/mainU9.js
+++ js/mainU9.js
@@ -1,28 +1,14 @@
-// initialize the configuration of map
 function initMap() {
-  // use JS's built-in Navigator to get user's lat/lng coordinates
-  navigator.geolocation.getCurrentPosition(function(position) {
-  //   // create an object to store lat/lng data
-    var userLocation = {
-      lat: 40.8054491,
-      lng: -73.9654415
-    };
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 40.8054491, lng: -73.9654415},
+    zoom: 8,
+    scrollwheel: false
+  });
 
-    // create a new instance of a map
-    // configure map with options object
-    var map = new google.maps.Map(document.getElementById('map'), {
-      center: userLocation,
-      zoom: 17,
-      scrollwheel: false
-    });
-
-    // use Marker constructor to add a marker to map
-    var marker = new google.maps.Marker({
-      position: userLocation,
-      map: map,
-      title: 'User Location'
-    });
-
+  var marker = new google.maps.Marker({
+    position: {lat: 40.8054491, lng: -73.9654415},
+    map: map,
+    title: 'Monks Café'
   });
 }
 
